dc6c8a300416c9fa737829f0a153ee28700f029e
Dennis Kleinhans <dennis.kleinhans991@gmx.de>
2025-05-15 17:48:17 +0200

    extract common operators like `Add`, `Sub`, etc. into seperate object `CommonOperators`

--- src/main/scala/compiler/AST.scala
+++ src/main/scala/compiler/AST.scala
@@ -1,7 +1,18 @@
 package compiler
 
-import compiler.LVar.UnaryOperator
-import compiler.LVar.BinaryOperator
+import compiler.CommonOperators.*
+
+object CommonOperators {
+
+  /** A unary operator represents an operation with a single operand. */
+  enum UnaryOperator:
+    case USub
+
+  /** A binary operator represents an operation with two operands. */
+  enum BinaryOperator:
+    case Add
+    case Sub
+}
 
 /** The abstract syntax tree (AST) for the LVar language.
   *
@@ -9,6 +20,7 @@ import compiler.LVar.BinaryOperator
   * interpretation or compilation.
   */
 object LVar {
+  import Expr.Identifier
 
   /** A module is the top-level program structure in LVar.
     *
@@ -23,7 +35,7 @@ object LVar {
   enum Stmt:
     case ExprStmt(e: Expr)
     case PrintStmt(e: Expr)
-    case AssignStmt(name: String, e: Expr)
+    case AssignStmt(id: Identifier, e: Expr)
 
   /** An expression in LVar can be a constant value, a unary operation, a binary
     * operation, or a function call.
@@ -32,17 +44,10 @@ object LVar {
     case Constant(n: Long)
     case UnaryOp(op: UnaryOperator, e: Expr)
     case BinaryOp(op: BinaryOperator, left: Expr, right: Expr)
-    case Call(name: String, args: List[Expr])
-    case Variable(name: String)
+    case Call(id: Identifier, args: List[Expr])
+    case Variable(id: Identifier)
+    case Identifier(name: String)
 
-  /** A unary operator represents an operation with a single operand. */
-  enum UnaryOperator:
-    case USub
-
-  /** A binary operator represents an operation with two operands. */
-  enum BinaryOperator:
-    case Add
-    case Sub
 }
 
 /** The abstract syntax tree (AST) for the LMonVar language.
@@ -51,6 +56,7 @@ object LVar {
   * interpretation or compilation.
   */
 object LMonVar {
+  import Expr.Identifier
 
   /** A module is the top-level program structure in LMonVar.
     *
@@ -63,7 +69,7 @@ object LMonVar {
     * operation or an assign operation.
     */
   enum Stmt:
-    case AssignStmt(name: String, rhs: Expr)
+    case AssignStmt(id: Identifier, rhs: Expr)
     case PrintStmt(a: Atom)
     case ExprStmt(e: Expr)
 
@@ -74,11 +80,12 @@ object LMonVar {
   enum Expr:
     case UnaryOp(op: UnaryOperator, a: Atom)
     case BinaryOp(op: BinaryOperator, lhs: Atom, rhs: Atom)
-    case Call(name: String, args: List[Atom])
+    case Call(id: Identifier, args: List[Atom])
     case AtomExpr(a: Atom)
+    case Identifier(name: String)
 
   /** An atom in LMonVar can be a constant value or a variable reference. */
   enum Atom:
     case Constant(n: Long)
-    case Variable(name: String)
+    case Variable(id: Identifier)
 }
