a9c82a41b28a52861804779f9da5233804f9265f
Dennis Kleinhans <dennis.kleinhans991@gmx.de>
2025-07-16 11:51:43 +0200

    refactore `NoOpElimination`

--- src/main/scala/optimization/NoOpElimination.scala
+++ src/main/scala/optimization/NoOpElimination.scala
@@ -5,6 +5,14 @@ import x86.Reg
 
 object NoOpElimination {
 
+  def eliminateNoOps(
+      progWithFuns: List[(String, x86.Program, Long)]
+  ): List[(String, x86.Program, Long)] = {
+    progWithFuns.map { case (name, program, stackSpace) =>
+      (name, eliminateNoOps(program), stackSpace)
+    }
+  }
+
   def eliminateNoOps(program: x86.Program): x86.Program = {
     val filteredBlocks = program.blocks.map { case (lable, instrs) =>
       val cleaned = instrs.filter {
